88920d9334ae26517db7b13a7e3d835a6d5033ce
aishangdren <104598891@qq.com>
2018-12-24 11:35:48 +0800

    ttt

--- src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
+++ src/main/java/com/bolingcavalry/mavendockerplugindemo/controller/Hello.java
@@ -11,6 +11,6 @@ public class Hello {
     @RequestMapping("/")
     public String sayHello(){
     	//修改返回的字符串的内容
-        return "123456. Hello jenkins21667j, " + new Date();
+        return "123456. Hello jenkins21d, " + new Date();
     }
 }
